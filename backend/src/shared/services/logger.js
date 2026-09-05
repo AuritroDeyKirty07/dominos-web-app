2087ebac1f6eae80500fa862bd5defaa30294632
Auritro Dey Kirty <deykirtyauritro@gmail.com>
2026-09-06 01:08:55 +0530

    Fix Winston logger for Vercel

--- backend/src/shared/services/logger.js
+++ backend/src/shared/services/logger.js
@@ -1,17 +1,24 @@
 import winston from "winston";
 
+const transports = [
+    new winston.transports.Console()
+];
+
+if (!process.env.VERCEL) {
+    transports.push(
+        new winston.transports.File({
+            filename: "logs/error.log",
+            level: "error",
+        })
+    );
+}
+
 export const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(
-    winston.format.errors({ stack: true }), // Preserves error stack traces
-    winston.format.timestamp(),
-    winston.format.json() // Stores clean JSON in log files
-  ),
-  transports: [
-    new winston.transports.File({
-      filename: "logs/error.log",
-      level: "error", // Only write errors to this file
-    }),
-    
-  ],
+    level: "info",
+    format: winston.format.combine(
+        winston.format.errors({ stack: true }),
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports,
 });
